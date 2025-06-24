c0d857fa7807d25f409d4cecbd4eb5470aa7f7f8
akashsamanta07 <as5740061@gmail.com>
2025-06-24 13:00:51 +0530

    Order placed function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,50 +50,50 @@ function Home() {
   return (
     <div className='bg-slate-300 w-full min-h-[100vh]'>
         <Nav prop={navchange} />
+        <div onClick={()=>{setOrder(0)}}>{/* for onclick */}
+          <div className='flex flex-wrap justify-center items-center gap-[0.75rem] m-[0.5rem] md:gap-[1.25rem] md:m-[1rem]'>
+            {cetagories.map((item,i)=>{
+            return( 
+              <div key={i} className='bg-white p-[0.6rem] md:p-[0.8rem] rounded-md shadow-md flex justify-center items-center flex-col gap-[3px] md:gap-1 hover:bg-green-200 hover:shadow-xl cursor-pointer transition-all duration-200' 
+              onClick={()=>{setCetagory(item.name)}}>
+                <div>
+                {item.image}
+                </div>
+                <div className='text-[0.75rem] md:text-[1rem] md:font-semibold'>
+                {item.name}
+                </div>
+              </div>
+            )})}
+          </div>
 
-        <div className='flex flex-wrap justify-center items-center gap-[0.75rem] m-[0.5rem] md:gap-[1.25rem] md:m-[1rem]'>
-          {cetagories.map((item,i)=>{
-           return( 
-            <div key={i} className='bg-white p-[0.6rem] md:p-[0.8rem] rounded-md shadow-md flex justify-center items-center flex-col gap-[3px] md:gap-1 hover:bg-green-200 hover:shadow-xl cursor-pointer transition-all duration-200' 
-            onClick={()=>{setCetagory(item.name)}}>
-              <div>
-              {item.image}
-              </div>
-              <div className='text-[0.75rem] md:text-[1rem] md:font-semibold'>
-              {item.name}
-              </div>
-            </div>
-          )})}
+            
+
+          <div className='w-[95%] md:w-[70%] mx-auto mt-[3rem] pb-[3rem] flex flex-wrap gap-[0.8rem] md:gap-[1.25rem] justify-center'>
+              { 
+                filter_food_items.length !==0 ?
+                    filter_food_items.map((item,i)=>{
+                      let itemset={
+                        id:item.id,
+                        food_name:item.food_name,
+                        food_type:item.food_type,
+                        food_quantity :item.food_quantity,
+                          food_image:item.food_image,
+                          price:item.price,
+                    }
+                      return (
+                        <Card 
+                          prop={itemset}
+                        key={i}/>
+                      )
+                    })
+                :
+                <div className='text-green-600 flex flex-col items-center p-4 md:text-[2rem]'>
+                    <MdMoodBad className='w-[2rem] h-[2rem] md:w-[4rem] md:h-[4rem] text-green-600'/>
+                    <p>no dish found</p>
+                </div>
+              }
+          </div>
         </div>
-
-          
-
-        <div className='w-[95%] md:w-[70%] mx-auto mt-[3rem] pb-[3rem] flex flex-wrap gap-[0.8rem] md:gap-[1.25rem] justify-center'>
-            { 
-              filter_food_items.length !==0 ?
-                  filter_food_items.map((item,i)=>{
-                    let itemset={
-                      id:item.id,
-                      food_name:item.food_name,
-                      food_type:item.food_type,
-                       food_quantity :item.food_quantity,
-                        food_image:item.food_image,
-                         price:item.price,
-                  }
-                    return (
-                      <Card 
-                        prop={itemset}
-                      key={i}/>
-                    )
-                  })
-              :
-              <div className='text-green-600 flex flex-col items-center p-4 md:text-[2rem]'>
-                  <MdMoodBad className='w-[2rem] h-[2rem] md:w-[4rem] md:h-[4rem] text-green-600'/>
-                  <p>no dish found</p>
-              </div>
-            }
-        </div>
-
           <Order prop={bucket}/>
         
     </div>
